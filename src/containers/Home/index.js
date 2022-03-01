4b45a686b7f574dcd42f05f442337e040e9673a1
Manish396 <59077192+Manish396@users.noreply.github.com>
2022-03-01 12:46:35 +0530

    first commit

---
+++ src/containers/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from "../../components/Layout"
+import "./style.css";
+export default function Home() {
+    return (
+        <div>
+            <Layout sidebar>
+            </Layout>
+        </div >
+    )
+}
