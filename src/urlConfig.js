8b52995106274c628cc056ce9b6319ce825fc2e7
Manish Kumar <Manish.123jbad@gmail.com>
2022-03-01 16:21:24 +0530

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseURL = location.hostname === "localhost" ? "http://localhost:8080" : "https://flipkart-backend-application.herokuapp.com";
+const baseURL = "https://flipkart-backend-application.herokuapp.com";
 export const api = `${baseURL}/api`;
 export const generatePublicURL = (filename) => {
     return `${baseURL}/public/${filename}`;
